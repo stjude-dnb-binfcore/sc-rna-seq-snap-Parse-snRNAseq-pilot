9e2d4bf8152b137d9a09fc6be265e93d203bb500
AntoniaChroni <achroni@noderome333.cm.cluster>
2026-03-06 17:24:34 -0600

    Fix code

--- analyses/integrative-analysis/util/custom-seurat-functions.R
+++ analyses/integrative-analysis/util/custom-seurat-functions.R
@@ -12,7 +12,7 @@
 plot_integrated_clusters = function (seurat_obj, palette, variable_value) { 
   ## take an integrated Seurat object, plot distributions over the specified variable
   
-  count_table <- table(seurat_obj@meta.data$seurat_clusters, seurat_obj@meta.data$variable_value)
+  count_table <- table(seurat_obj@meta.data$seurat_clusters, seurat_obj@meta.data[[variable_value]])
   count_mtx   <- as.data.frame.matrix(count_table)
   count_mtx$cluster <- rownames(count_mtx)
   melt_mtx    <- melt(count_mtx)
